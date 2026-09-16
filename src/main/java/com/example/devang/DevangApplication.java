519816c4deb0afdf6aa65c8ac0d7b08ffb5974fc
Devang-Deokule <devangdeokule26@gmail.com>
2026-09-16 22:37:04 +0530

    Initial commit

---
+++ src/main/java/com/example/devang/DevangApplication.java
@@ -0,0 +1,14 @@
+package com.example.devang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevangApplication.class, args);
+	}
+
+
+}
